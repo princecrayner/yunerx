c0d1a0147929ac4fc54d2506c783c75321f2552c
princecrayner <princecrayner@gmail.com>
2026-07-07 21:08:50 +0000

    project updated

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,13 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 
+console.log("CLOUD_NAME:", process.env.CLOUD_NAME);
+console.log("CLOUD_API_KEY:", process.env.CLOUD_API_KEY ? "FOUND" : "MISSING");
+console.log("CLOUD_API_SECRET:", process.env.CLOUD_API_SECRET ? "FOUND" : "MISSING");
+
 cloudinary.config({
-
     cloud_name: process.env.CLOUD_NAME,
-
     api_key: process.env.CLOUD_API_KEY,
-
     api_secret: process.env.CLOUD_API_SECRET
-
 });
 
 module.exports = cloudinary;
